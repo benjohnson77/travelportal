8206b027e8eca2b1b8008b2e29dcb04b032568a3
benjohnson77 <bjohnson@ubersoftware.com>
2014-07-02 19:21:42 -0500

    test gitignore

---
+++ wp-content/themes/lobster/tour_cat.php
@@ -0,0 +1,11 @@
+<div class="col-md-offset-1 col-md-5">
+  <div class="panel panel-primary">
+    <div class="panel-heading">
+      <i class=""></i><h3 class="panel-title">Popular Tour Destinations</h3>
+    </div>
+    <div class="panel-body">
+        <?php wp_list_categories(''); ?>
+    </div>
+  </div>
+</div>
+
